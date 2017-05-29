f6274d7e08518b5fc639a8d5fbbbf46f03ba0f26
mlacaud <matlaca24@gmail.com>
2017-05-29 23:19:17 +0200

    change type of calendar to month

--- js/calendar.js
+++ js/calendar.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	
+
 		$('#calendar').fullCalendar({
 
 			header: {
@@ -10,26 +10,26 @@ $(document).ready(function() {
 
 			displayEventTime: true, // don't show the time column in list view
 
-            defaultView: 'listYear',
+      defaultView: 'month',
 
 			// THIS KEY WON'T WORK IN PRODUCTION!!!
 			// To make your own Google API key, follow the directions here:
 			// http://fullcalendar.io/docs/google_calendar/
 			googleCalendarApiKey: 'AIzaSyARf5liewf2n1ShLzS0O7KcPgd0g_WtGVM',
-		
+
 			// US Holidays
 			events: 'http://www.google.com/calendar/feeds/contact%40oldjoeskaya.fr/public/basic',
-			
+
 			eventClick: function(event) {
 				// opens events in a popup window
 				window.open(event.url, 'gcalevent', 'width=700,height=600');
 				return false;
 			},
-			
+
 			loading: function(bool) {
 				$('#loading').toggle(bool);
 			}
-			
+
 		});
-		
+
 	});
