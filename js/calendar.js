5a582a6857ac7d48503fff2fc0dc4edf46d68711
mlacaud <matlaca24@gmail.com>
2017-05-29 21:02:42 +0200

    Improve website with calendar

---
+++ js/calendar.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	
+		$('#calendar').fullCalendar({
+
+			header: {
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,listYear'
+			},
+
+			displayEventTime: true, // don't show the time column in list view
+
+            defaultView: 'listYear',
+
+			// THIS KEY WON'T WORK IN PRODUCTION!!!
+			// To make your own Google API key, follow the directions here:
+			// http://fullcalendar.io/docs/google_calendar/
+			googleCalendarApiKey: 'AIzaSyARf5liewf2n1ShLzS0O7KcPgd0g_WtGVM',
+		
+			// US Holidays
+			events: 'http://www.google.com/calendar/feeds/contact%40oldjoeskaya.fr/public/basic',
+			
+			eventClick: function(event) {
+				// opens events in a popup window
+				window.open(event.url, 'gcalevent', 'width=700,height=600');
+				return false;
+			},
+			
+			loading: function(bool) {
+				$('#loading').toggle(bool);
+			}
+			
+		});
+		
+	});
